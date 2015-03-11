96af58f59140de8f26faf28201148efb311ce0c2
JP Smith <jp.smith@ilstu.edu>
2015-03-11 09:17:04 -0500

    updated screenwriter

--- tools/Screenwriter.hs
+++ tools/Screenwriter.hs
@@ -2,17 +2,27 @@ module Screenwriter where
 
 pre :: String
 pre = unlines [ "#!/bin/bash"
+              , ""
+              -- Get rid of remnants of last level
               , "clear"
+              , "unalias -a"
+              , ""
+              -- For pretty text
               , "red=\'\\033[0;31m\'"
               , "NC=\'\\033[0m\'"
               ]
 
+-- The message that shows at the start of the level, ideally giving some
+-- information about the solution
 motd :: String -> String
 motd message = "echo -e \"${red}" ++ message ++ "${NC}\""
 
+-- Move in new files. Note that the behavior of copy here will overwrite
+-- old files with the same name, but not all files
 copy :: String -> Int -> String
 copy course level = "cp -a /resources/" ++ course ++ "/level_" ++ show level ++ "/. ./"
 
+-- Set up messages to show on first use of commands
 aliases :: [(String, String)] -> String
 aliases [] = ""
 aliases ((command, message) : rest) =
@@ -21,6 +31,7 @@ aliases ((command, message) : rest) =
           , "  echo -e \"${red}" ++ message ++ "${NC}\""
           , "  echo"
           , "}"
+          -- The `&& command` assures that flags will get passed through
           , "alias " ++ command ++ "=\'" ++ command ++ "fun &&" ++ command ++ "\'"
           ] ++ "\n" ++ aliases rest
 
